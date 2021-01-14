1ed00ade991dfc9a993e945298bea3a4a41827b6
Youssef Benabdallah <youssefbenabdallah@hotmail.fr>
2021-01-14 19:35:35 +0100

    donee

---
+++ app/javascript/blog/index.jsx
@@ -0,0 +1,46 @@
+// external modules
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { HashRouter, Route, Switch }
+  from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import { reducer as formReducer } from 'redux-form';
+
+// internal modules
+import PostsIndex from './containers/posts_index';
+import PostsNew from './containers/posts_new';
+import PostsShow from './containers/posts_show';
+// import '../assets/stylesheets/application.scss';
+import postsReducer from './reducers/posts_reducer';
+// State and reducers
+const reducers = combineReducers({
+  posts: postsReducer,
+  form: formReducer
+});
+
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+const root = document.getElementById('root');
+const initialState = {
+  posts: JSON.parse(root.dataset.posts)
+}
+// render an instance of the component in the DOM
+ReactDOM.render(
+  <Provider store={createStore(reducers, initialState, middlewares)}>
+    <HashRouter history={createBrowserHistory}>
+      <div className="thin-container">
+        <Switch>
+          <Route path="/" exact component={PostsIndex} />
+          <Route path="/posts/new" exact component={PostsNew} />
+          <Route path="/posts/:id" component={PostsShow} />
+        </Switch>
+      </div>
+    </HashRouter>
+  </Provider>,
+  document.getElementById('root')
+);
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/posts/new", to: 'pages#home'
+  get "/posts/:id", to: 'pages#home'
+
+  # API
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do # creating a version so we can create other version later and keep all kind of clients
+      resources :posts, only: [:index, :show, :create]
+    end
+  end
 end
